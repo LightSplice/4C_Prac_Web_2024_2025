d6c2ce53dcc17f0b6680844785cd412084c89ebf
LightSplice <112875133+LightSplice@users.noreply.github.com>
2024-02-02 12:25:58 +0100

    Add files via upload

---
+++ 02.02.2024/Zadanie 1.php
@@ -0,0 +1,14 @@
+Metoda GET
+Wady:
+- Można ujawnić dane prywatne użytkownika w adresie URL.
+Zalety:
+- Nie potrzeba formularza aby zmodyfikować zawartość tych zmiennych.
+- Łatwo dodać taką stronę do "ulubionych" ponieważ zakładka będzie kierować wprost do strony z wynikami wyszukiwania.
+
+Metoda POST
+Wady:
+- Nie można łączyć się z parametrami.
+- Nie można indeksować.
+Zalety:
+- Poufnie przekazuje dane.
+- Dobrze się nadaje do rozbudowanych formularzy.
---
+++ 02.02.2024/Zadanie 2.php
@@ -0,0 +1,4 @@
+<?php
+    echo $_GET['imie'] . "<br>";
+    echo $_GET['nazwisko'];
+?>
